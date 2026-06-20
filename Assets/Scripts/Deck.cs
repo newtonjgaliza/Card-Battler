0080cb2e6f86d118ec4679f9feee2b88fbe9cffd
newton <newtonjgaliza@gmail.com>
2026-06-20 19:24:16 -0300

    lesson 1.8

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 public class Deck : MonoBehaviour
 {
     [SerializeField] private List<CardData> drawPile = new List<CardData>();
+
+    [SerializeField] private GameObject cardBack;
     
+    private const float VERTICAL_SPACING = .1f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log(DrawCard());
+        DeckDrawVisuals();
     }
 
     // Update is called once per frame
@@ -28,5 +32,14 @@ public class Deck : MonoBehaviour
             return data;
         }
         return null;
+    }
+
+    private void DeckDrawVisuals()
+    {
+        for(int i = 0; i < drawPile.Count; i++)
+        {
+            GameObject newCardBack = Instantiate(cardBack, transform);
+            newCardBack.transform.localPosition = new Vector3(0f, -i * VERTICAL_SPACING, 0f);
+        }
     }
 }
