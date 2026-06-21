5d21c323689048cbeb47ce983bd7483ee921b8a6
newton <newtonjgaliza@gmail.com>
2026-06-21 00:23:18 -0300

    lesson 1.13

---
+++ Assets/Scripts/PlayzoneTrigger.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayzoneTrigger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent(out Card card))
+        {
+            Debug.Log("CARD entered");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent(out Card card))
+        {
+            Debug.Log("CARD left");
+        }
+    }
+}
